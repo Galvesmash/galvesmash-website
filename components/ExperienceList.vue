aa1dbc77d95661e365cf415cbd3ce94b39b284cc
Gabriel Galves Taliatti <galvesmash.dev@gmail.com>
2024-09-26 16:06:33 -0300

    feat: better  experiences texts

--- components/ExperienceList.vue
+++ components/ExperienceList.vue
@@ -13,49 +13,49 @@
         end: '',
         role: 'Frontend Engineer',
         start: '04-01-2024',
-        text: 'Working as a Frontend Engineer with high knowledge in JavaScript, TypeScript, Vue.js, Nuxt, Tailwind CSS, SEO, Jest, Vue-Test-Utils.\nResponsible for developing and the creation of new features in the register part for the main website at Atlas Technologies in Vue.js, JavaScript, TypeScript, Nuxt, Tailwind CSS, Jest, API integration and SEO optimization.'
+        text: 'Working as a Frontend engineer with JavaScript, TypeScript, Vue.js, Nuxt, Tailwind CSS, SASS, BEM methodology, SEO, Jest, Vue-Test-Utils.\nResponsible for developing and the creation of new features in the register part for the main website at Atlas Technologies in Vue.js, JavaScript, TypeScript, Nuxt, Tailwind CSS, Jest, API integration, SEO optimization and responsible design.'
       },
       {
         company: 'Teladoc Health',
         end: '11-01-2023',
         role: 'Frontend Developer',
         start: '10-01-2020',
-        text: 'Worked as a Frontend Developer with high knowledge in Javascript, Vue.js, Vuetify.js, Jest, Vue-Test-Utils.\nAlso helping the Backend team with PHP and Laravel.\nResponsible for developing and the creation of new features for the website of Teladoc Health in VueJS with VuetifyJs and integrating both of them, I’m also responsible for the development and maintaining a Rest API with microservices in Laravel.'
+        text: 'Worked as a Frontend developer with Javascript, Vue.js, Vuetify.js, Jest, Vue-Test-Utils.\nAlso helping the Backend team with PHP and Laravel.\nResponsible for developing and the creation of new features for the website of Teladoc Health in Vue.js, Vuetify.js, SASS, responsible design and tests. I was also responsible for the development and maintaining a Rest API with microservices in Laravel.'
       },
       {
         company: 'Tem Saúde',
         end: '10-01-2020',
         role: 'Full Stack Developer',
         start: '11-01-2019',
-        text: 'Worked as a Full Stack Developer with High knowledge in ReactJS, NextJS and NodeJS.\nResponsible for creating and developing a Rest API with microservices in NodeJS, developing a responsible frontend in ReactJS with NextJS and integrating both of them.'
+        text: 'Worked as a Full Stack developer with React, Next.js and Node.js.\nResponsible for developing the frontend in React with Next.js, SASS and responsible design. Creating and developing a Rest API with microservices in Node.js with GraphQ. And responsible for the integration.'
       },
       {
         company: 'Enext Consultoria',
         end: '11-01-2019',
         role: 'Full Stack Developer',
         start: '07-01-2019',
-        text: 'Full Stack Developer with High knowledge in VueJS, NuxtJS and LESS.\nResponsible for leading the Suvinil "Encontre seu Pintor" project, implementing features in VueJs, design and integrating a rest API.\nAlso helped in the Project Imply from "Bondinho Pão de Açúcar" by creating and developing the pages and designs in VueJs, NuxtJs and SCSS.'
+        text: 'Full Stack developer with Vue.js, Nuxt.js and LESS.\nResponsible for leading the Suvinil "Encontre seu Pintor" project, implementing features in Vue.js and integrating a rest API.\nAlso helped in the Project Imply from "Bondinho Pão de Açúcar" by creating and developing the pages and designs in Vue.js, Nuxt.js and SASS.'
       },
       {
         company: '2Mundos',
         end: '07-01-2019',
         role: 'Full Stack  Developer',
         start: '12-01-2018',
-        text: 'Full Stack Developer for 2Toring (2BeLive) product from 2Mundos. High knowledge in VueJS and LESS for Frontend and learning about PHP with Laravel for the Backend.\nResponsible for guiding a team, creation and integration of the Rest API, implementing features and integrating all the new ideas for the 2BeLive learning platform. Participation in implementations using Pusher and Opentok for the 2BeLive Live Session.'
+        text: 'Full Stack developer for 2Toring product from 2Mundos. As a Frontend I worked with JavaScript, Vue.js, Vuetify.js, SASS and responsible design. As a Backend, I was learning PHP, Laravel and Microservices.\nResponsible for guiding a team, creation and integration of the Rest API, implementing features and integrating all the new ideas for the 2BeLive learning platform.\nParticipation in implementations using Pusher and Opentok for the 2Toring’s Live Session.'
       },
       {
         company: '2Mundos',
         end: '12-01-2018',
         role: 'Frontend Developer',
         start: '10-01-2016',
-        text: 'I worked as a frontend developer for TutorIQ (Owned by Age of Learning) with work trips in LA for TutorIQ assistance at Age of Learning itself. High knowledge in AngularJS and SCSS. I also worked on integrating tasks and features involving Cordova, Ionic, Surfly and Opentok for the Livesession part.'
+        text: 'Frontend developer for TutorIQ product (owned by Age of Learning) with work trips in LA for TutorIQ assistance at Age of Learning HQ. Worked with Angular.js 1.6, SASS, responsible design and BEM methodology. I also worked on tasks and features involving Cordova, Ionic, Surfly and Opentok for the TutorIQ’s Live Session.'
       },
       {
         company: '2Mundos',
         end: '03-01-2017',
         role: 'Unity3D Developer',
         start: '03-01-2016',
-        text: 'Unity3D developer with high C# knowledge, worked for the ETE FMC Residential Electrical Course project for mobile phones.'
+        text: 'Worked as a Unity3D developer for the ETE FMC Residential Electrical Course project for mobile phones. Worked with Unity3D and C#'
       },
       {
         company: 'Game e Arte',
@@ -77,7 +77,7 @@
         class="ma-0"
       >
         <v-col
-          class="mb-4 mb-md-0 pa-0 pr-md-3 pr-lg-6 text-md-right"
+          class="mb-4 mb-md-0 pa-0 pr-md-6 text-md-right"
           color="secondary"
           cols="12"
           md="3"
@@ -93,7 +93,7 @@
           cols="12"
           md="9"
         >
-          <p class="text-secondary">{{ experience.text }}</p>
+          <p class="text-headline-general text-secondary">{{ experience.text }}</p>
         </v-col>
 
         <v-divider v-if="index < experienceList.length-1" class="my-4 my-md-6" />
